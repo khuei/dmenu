5652265dccd165be0c44189d4e448dbc28288326
Khue Nguyen <Z5483Y@gmail.com>
2020-08-13 18:16:43 -0400

    Modify default configuration
    - Increase font size to 14
    - Change colorscheme
    - Increase number of line on dmenu window to 20

--- config.def.h
+++ config.def.h
@@ -6,20 +6,18 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 700;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
+static const char *fonts[] = { "monospace:size=14" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#D8D8D8", "#181818" },
+	[SchemeSel] = { "#181818", "#D8D8D8" },
+	[SchemeSelHighlight] = { "#F7CA88", "#D8D8D8" },
+	[SchemeNormHighlight] = { "#F7CA88", "#181818" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 20;
 
 /*
  * Characters not considered part of a word while deleting words
